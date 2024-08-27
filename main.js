6e0df06cad3757d0bf20704d085c9241726a5685
Hyunnism <muhamad.hidayat.setiawan@gmail.com>
2024-08-27 16:51:22 +0700

    latest

--- main.js
+++ main.js
@@ -14,3 +14,22 @@ navLinks.addEventListener("click", (e) => {
     navLinks.classList.remove("open");
     menuBtnIcon.setAttribute("class", "ri-menu-4-line");
 });
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.getElementById("orderForm");
+    const button = form.querySelector(".animated-button");
+    const overlay = document.querySelector(".overlay");
+
+    form.addEventListener("submit", function (event) {
+        event.preventDefault(); // Prevent the form from submitting immediately
+        
+        button.classList.add("clicked");
+        overlay.classList.add("active");
+        
+        // Wait for the animation to complete before redirecting
+        setTimeout(function () {
+            window.location.href = "form.html";
+        }, 500); // Match the duration of the animation
+    });
+});
