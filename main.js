679998115b43e93d43ab2d3ac39bfce66f7f885d
Hyunnism <muhamad.hidayat.setiawan@gmail.com>
2024-08-28 15:51:00 +0700

    28/08/2024

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ menuBtn.addEventListener("click", (e) => {
     navLinks.classList.toggle("open");
 
     const isOpen = navLinks.classList.contains("open");
-    menuBtnIcon.setAttribute("class", isOpen? "ri-close-line"  :  "ri-menu-4-line");
+    menuBtnIcon.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-4-line");
 });
 
 
@@ -23,13 +23,35 @@ document.addEventListener("DOMContentLoaded", function () {
 
     form.addEventListener("submit", function (event) {
         event.preventDefault(); // Prevent the form from submitting immediately
-        
+
         button.classList.add("clicked");
         overlay.classList.add("active");
-        
+
         // Wait for the animation to complete before redirecting
         setTimeout(function () {
             window.location.href = "form.html";
         }, 500); // Match the duration of the animation
+    });
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const counters = document.querySelectorAll('.count');
+    const speed = 200; // Kecepatan animasi
+
+    counters.forEach(counter => {
+        const updateCount = () => {
+            const target = +counter.getAttribute('data-target');
+            const count = +counter.innerText;
+            const increment = target / speed;
+
+            if (count < target) {
+                counter.innerText = Math.ceil(count + increment);
+                setTimeout(updateCount, 1);
+            } else {
+                counter.innerText = target;
+            }
+        };
+
+        updateCount();
     });
 });
